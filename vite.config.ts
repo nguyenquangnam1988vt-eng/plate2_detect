e23d476f87d1df11a922b315c80d4881b112010b
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-04-17 15:29:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Giúp load assets đúng khi chạy trên GitHub Pages
+  plugins: [react(), tailwindcss()],
+  base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './'),
     },
   },
 });
