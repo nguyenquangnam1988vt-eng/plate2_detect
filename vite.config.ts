74e9cb128b910a43c3b2bfc0eb8fa76137b1cdb0
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-04-17 15:34:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['onnxruntime-web'],
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
 });
