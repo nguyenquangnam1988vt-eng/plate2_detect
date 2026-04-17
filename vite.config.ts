1ad5b621c73dd6f3f58aeb968a68470c96ddc419
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-04-17 15:41:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/plate2_detect/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
